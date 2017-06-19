2b6940229d833bcba9c234c336c79b083c7211f8
Vadym <bormvad@gmail.com>
2017-06-19 08:55:45 +0300

    Add files via upload

---
+++ calendar.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10.10
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Июн 19 2017 г., 08:55
+-- Версия сервера: 5.5.45
+-- Версия PHP: 5.6.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `calendar`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `events`
+--
+
+CREATE TABLE IF NOT EXISTS `events` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `description` text NOT NULL,
+  `author` text NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `color` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Дамп данных таблицы `events`
+--
+
+INSERT INTO `events` (`id`, `name`, `date`, `time`, `description`, `author`, `status`, `color`) VALUES
+(1, 'event1', '2017-06-01', '00:00:00', 'descriprion1', 'author1', 1, 'blue'),
+(2, 'event2', '2017-06-21', '00:00:12', 'desc2', 'auth2', 1, 'green'),
+(3, 'event3', '2017-07-12', '00:00:12', 'desc3', 'auth3', 1, 'red'),
+(4, 'event4', '2017-06-12', '03:02:00', 'desc4', '', 0, '#00ffff'),
+(5, 'event5', '2017-07-01', '03:04:00', 'desc5', '', 0, '#000000');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` text NOT NULL,
+  `password` text CHARACTER SET utf8mb4 NOT NULL,
+  `prava` int(11) NOT NULL,
+  `online` int(11) NOT NULL,
+  `last_act` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `password`, `prava`, `online`, `last_act`) VALUES
+(1, 'admin', 'admin', 1, 0, 1497820548),
+(2, 'user', 'user', 0, 0, 1497818553);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
